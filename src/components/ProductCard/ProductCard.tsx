21cceb5ea4516e7eefa4e4fa80f3fc3322ea8315
TanishqSingla <tanishqsingla00@gmail.com>
2022-12-26 19:21:24 +0530

    Add discount pill icon to card

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,4 +1,4 @@
-import { Card, CardMedia, Chip } from "@mui/material";
+import { Card, CardMedia, Chip, Paper, Typography } from "@mui/material";
 
 interface CardProps {
 	id?: string;
@@ -22,14 +22,39 @@ export default function (props: CardProps) {
 				borderRadius: "12px",
 			}}
 		>
-			<div style={{ borderRadius: "12px", overflow: "hidden" }}>
+			<div
+				style={{
+					borderRadius: "12px",
+					overflow: "hidden",
+					position: "relative",
+				}}
+			>
 				<CardMedia
 					height="180px"
 					component="img"
 					src="https://images.unsplash.com/photo-1664575602276-acd073f104c1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
 					alt={`${props.title} image`}
 				/>
-				{props.discount && <div></div>}
+				{props.discount && (
+					<Paper
+						sx={{
+							height: 18,
+							fontSize: 12,
+							textAlign: "center",
+							borderRadius: 12,
+							fontWeight: 600,
+							fontFamily: "Poppins",
+							width: 54,
+							color: "#6A983C",
+							background: "#F4F8EC",
+							position: "absolute",
+							top: 8,
+							left: 8,
+						}}
+					>
+						- {props.discount} %
+					</Paper>
+				)}
 			</div>
 		</Card>
 	);
